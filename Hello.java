fcf2681226b07648f672db2271526247a4af8ac3
Houssem Eddine Lassoued <houssemeddine.lassoued@gmail.com>
2022-04-02 14:59:08 +0200

    beaucoup de changements

--- Hello.java
+++ Hello.java
@@ -1,9 +1,12 @@
+import java.lang.String;
+
 public class Hello{
-  
-  
  public static void main(){
   System.out.println("Hello");
   //affichage de Hello
  }
-  
+  public int fonction3(){
+	  ////.....
+	  return 0;
+	  }
 }
